19d91ba86d98c234fbe976758e577b0653ab94fa
Roman <malivskiy@gmail.com>
2022-04-01 00:56:20 +0300

    strANDtext

--- scriptConsole.js
+++ scriptConsole.js
@@ -1,8 +1,48 @@
-"use strict";
+// "use strict";
 
-function showFirstMessage(text) {
-  console.log(text);
-  let num = 20;
-}
+// Function
+// let num = 20;
 
-showFirstMessage("hello");
+// function showFirstMessage(text) {
+//   console.log(text);
+//   let num = 10;
+// }
+
+// showFirstMessage("hello");
+// console.log(num);
+
+// // function calc(a, b) {
+// //   return a + b;
+// // }
+
+// // console.log(calc(4, 3));
+
+// function ret() {
+//   let num = 50;
+//   return num;
+// }
+
+// const anotherNum = ret();
+
+// console.log(anotherNum);
+
+
+// const logger = function() {
+//   console.log('hello');
+// };
+// logger();
+
+
+// const calc = (a, b) => a + b;
+
+
+
+//-----------------------------------------------------------------------------------------------------
+
+
+//string and number
+
+const str = `text`;
+const num = 14;
+
+console.log(str.toUpperCase());
