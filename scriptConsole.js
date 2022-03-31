affc6a8ec1271be8967c4beeb106b65cfcbc9653
Roman <malivskiy@gmail.com>
2022-04-01 00:17:03 +0300

    nextTaskArrov

--- scriptConsole.js
+++ scriptConsole.js
@@ -1,37 +1,8 @@
-/* Задание на урок:
+"use strict";
 
-1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+function showFirstMessage(text) {
+  console.log(text);
+  let num = 20;
+}
 
-2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
-отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит -
-возвращаем пользователя к вопросам опять
-
-3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
-"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше -
-"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
-
-4) Потренироваться и переписать цикл еще двумя способами*/
-
-
-'use strict';
-
-
-const numberOfFilms = 4;
-
-const personalMovieDB = {
-  count: numberOfFilms,
-  movies: {},
-  actors: {},
-  genres: [],
-  privat: false
-};
-
-const lastSeenFilm_1 = "Matrix",
-  lastSeenFilmMark_1 = 8,
-  lastSeenFilm_2 = "Speed",
-  lastSeenFilmMark_2 = 7;
-
-personalMovieDB.movies[lastSeenFilm_1] = lastSeenFilmMark_1;
-personalMovieDB.movies[lastSeenFilm_2] = lastSeenFilmMark_2;
-
-console.log(personalMovieDB);
+showFirstMessage("hello");
