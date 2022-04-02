a1371942f1b269335493c3cdc33c0e90b6d09436
Roman <malivskiy@gmail.com>
2022-04-02 20:46:37 +0300

    tastRefDone

--- refactoringFirst.js
+++ refactoringFirst.js
@@ -12,6 +12,85 @@
 при помощи метода forEach вывести в консоль сообщения в таком виде:
 "Любимый жанр #(номер по порядку, начиная с 1) - это (название из массива)"*/
 
-'use strict';
+
 
 // Код возьмите из предыдущего домашнего задания
+'use strict';
+
+
+const personalMovieDB = {
+  count: null,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+
+  start() {
+    this.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+    while (this.count == null || this.count == '' || isNaN(this.count)) {
+      this.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+    return this;
+  },
+  rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+      let a;
+      let b;
+      do {
+        a = prompt('Один из последних просмотренных фильмов?', '');
+      } while (a === null || a === '' || a.length > 50);
+      do {
+        b = prompt('На сколько оцените его?', '');
+      } while (b === null || b === '' || b.length > 50);
+      this.movies[a] = b;
+    }
+    return this;
+  },
+  detectPersonalLevel() {
+    if (this.count < 10) {
+      alert('Просмотрено довольно мало фильмов');
+    } else if (this.count < 30 && this.count > 10) {
+      alert('Вы классический зритель!');
+    } else if (this.count > 30) {
+      alert('Вы киноман');
+    } else {
+      alert("Произошла ошибка");
+    }
+    return this;
+  },
+  showMyDB() {
+    if (this.privat == false) {
+      console.log(this);
+    }
+    return this;
+  },
+  writeYourGenres() {
+    for (let i = 1; i < 4; i++) {
+      let genre;
+      while (true) {
+        genre = prompt(`Ваш любимый жанр под номером ${i}`, '');
+        if (genre !== null && genre != '') break;
+      }
+      this.genres.push(genre);
+    }
+    this.genres.forEach((genre, i) => console.log(`Любимый жанр ${i + 1} - это ${genre}`));
+    return this;
+  },
+  toggleVisibleMyDB() {
+    this.privat = !this.privat;
+    return this;
+  }
+
+};
+personalMovieDB
+  .start()
+  .rememberMyFilms()
+  .detectPersonalLevel()
+  .showMyDB()
+  .writeYourGenres()
+  .toggleVisibleMyDB();
+// console.log(true);
+// console.log(!true);
+
+
